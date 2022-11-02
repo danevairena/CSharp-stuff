963c143735c94560e2549be934bdd49e52969390
Irena Daneva <de.luxe9997@gmail.com>
2022-11-02 13:54:47 +0200

    Update RandomNumbersGenerator.cs

--- RandomNumbersGenerator.cs
+++ RandomNumbersGenerator.cs
@@ -8,21 +8,33 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
+            //prompt user for number of needed random numbers
             Console.WriteLine("How many random numbers you want:");
             int numNumbers = int.Parse(Console.ReadLine());
+            
+            //prompt for lower bound
             Console.WriteLine("Lower bound:");
             int lowerBound = int.Parse(Console.ReadLine());
+            
+            //prompt for upper bound
             Console.WriteLine("Upper bound:");
             int upperBound = int.Parse(Console.ReadLine());
+            
+            //check for correct user input
             if (lowerBound > upperBound)
             {
                 Console.WriteLine("Incorrect range.");
                 return;
             }
+            
+            //creating array for the random numbers
             Random rNum = new Random();
             int[] numArray = new int[numNumbers];
+            
+            //loop for generating random numbers
             for(int i = 0; i < numNumbers; i++)
             {
+                //generating random number between lower and upper bound
                 numArray[i] = rNum.Next(0,100);
                 Console.WriteLine(numArray[i]);
             }
