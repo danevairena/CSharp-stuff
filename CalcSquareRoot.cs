e29fb9539992d96ee0082670cbb961c95f3d6a44
Irena Daneva <de.luxe9997@gmail.com>
2022-11-02 13:51:21 +0200

    Update CalcSquareRoot.cs

--- CalcSquareRoot.cs
+++ CalcSquareRoot.cs
@@ -6,9 +6,14 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
+            //prompt user for number
             Console.WriteLine("Enter number to calculate the square root:\n");
             double number = double.Parse(Console.ReadLine());
+            
+            //calc square root
             double squareRoot = Math.Sqrt(number);
+            
+            //output
             Console.WriteLine($"Square root of {number} is {squareRoot}");
         }
     }
