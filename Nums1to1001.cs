a4d069701a6b62614236c9f5452c67940ff06d25
Irena Daneva <de.luxe9997@gmail.com>
2022-11-02 08:54:50 +0200

    Create Nums1to1001.cs

---
+++ Nums1to1001.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 1001; i++)
+            {
+                Console.WriteLine($"{i}\n");
+            }
+        }
+    }
+}
